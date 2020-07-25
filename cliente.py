84940764c1c7c80dbaf4c4828268d0c7f7c3bd32
FireSoul512 <54947216+FireSoul512@users.noreply.github.com>
2020-07-25 13:27:33 -0500

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,10 +1,10 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect( ('localhost',7080) )
+s.connect( ('localhost',7080) ) #ip de donde se encuentra el servidor y el puerto por el que escucha
 
-mensaje = input('Introduce el mensaje >>  ')
-s.send(bytes(mensaje, "utf-8"))
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
-s.close()
+mensaje = input('Introduce el mensaje >>  ') 
+s.send(bytes(mensaje, "utf-8")) #Aqui se inserta el mensaje que se envia a el server
+msg = s.recv(1024) # Se obtiene la respuesta del server
+print(msg.decode("utf-8")) #Se muestra la respuesta del server decodificando con utf-8
+s.close() # Se cierra la connecion con el server
