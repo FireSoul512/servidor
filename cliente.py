45b4ddcc1287f20e61900d38560401cc33b8e79f
FireSoul512 <54947216+FireSoul512@users.noreply.github.com>
2020-07-25 12:10:49 -0500

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,9 +1,10 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect( ('192.168.1.72',8000) )
+s.connect( ('localhost',7080) )
 
-s.send(bytes("si", "utf-8"))
+mensaje = input('Introduce el mensaje >>  ')
+s.send(bytes(mensaje, "utf-8"))
 msg = s.recv(1024)
 print(msg.decode("utf-8"))
 s.close()
