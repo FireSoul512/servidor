b610c60edb069acd250e03829e3ed3257b2ba32a
FireSoul512 <54947216+FireSoul512@users.noreply.github.com>
2020-07-24 23:42:25 -0500

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,7 +1,7 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect( ('189.148.85.231',8000) )
+s.connect( ('192.168.1.72',8000) )
 
 s.send(bytes("si", "utf-8"))
 msg = s.recv(1024)
