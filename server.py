1f918bd008d525a4051638db2d3342b9517a2852
FireSoul512 <54947216+FireSoul512@users.noreply.github.com>
2020-07-25 12:10:54 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,20 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind( ('localhost',8000) )
+s.bind( ('localhost',7080) )
 s.listen(5)
+ciclo = True
+print('Para cerrar el server el cliente tiene que introducir SALIR')
 
-while True:
+while ciclo:
     conexion, addr = s.accept()
-    print("Nueva conexion")
+    print("Nueva conexion",addr)
     print(addr)
     conexion.send(bytes("Hola como estas", "utf-8"))
     msg = conexion.recv(1024)
     print(msg.decode("utf-8"))
     conexion.close()
+    if msg.decode("utf-8") == "SALIR":
+        ciclo = False
+
+print('Adios bro XD')
